2545137d52290fa0bc5af9082b01205730cd19c7
zhangeldy01 <shakhanov_zhan@mail.ru>
2025-06-16 12:28:05 +0300

    Added folder sorting

--- src/main/java/homeWork5/Tree.java
+++ src/main/java/homeWork5/Tree.java
@@ -1,6 +1,8 @@
 package homeWork5;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Класс для вывода дерева директорий и файлов.
@@ -43,7 +45,7 @@ public class Tree {
         System.out.print(indent);
         if (isLast) {
             System.out.print("└─");
-            indent += "  ";
+            indent += " ";
         } else {
             System.out.print("├─");
             indent += "│ ";
@@ -54,6 +56,37 @@ public class Tree {
         if (files == null)
             return;
 
+        // Сортируем файлы и директории
+        Arrays.sort(files, (file1, file2) -> {
+            boolean isDir1 = file1.isDirectory();
+            boolean isDir2 = file2.isDirectory();
+
+            // Приоритет для директорий (сначала директории)
+            if (isDir1 && !isDir2) {
+                return -1; // file1 - директория, file2 - файл (file1 раньше)
+            }
+            if (!isDir1 && isDir2) {
+                return 1; // file1 - файл, file2 - директория (file2 раньше)
+            }
+
+            // Если оба файла или обе директории, сортируем по имени
+            return file1.getName().compareTo(file2.getName());
+
+        });
+
+        // Специальная сортировка для .idea, backup, src, target
+        Arrays.sort(files, (file1, file2) -> {
+           String name1 = file1.getName();
+           String name2 = file2.getName();
+
+           if (file1.isDirectory() && file2.isDirectory()) {
+               if (name1.equals(".idea") && !name2.equals(".idea")) return -1;
+               if (name1.equals("backup") && !name2.equals(".idea") && !name2.equals("backup")) return -1;
+               if (name1.equals("src") && !name2.equals(".idea") && !name2.equals("backup") && !name2.equals("src")) return -1;
+               if (name1.equals("target") && !name2.equals(".idea") && !name2.equals("backup") && !name2.equals("src") && !name2.equals("target")) return -1;
+           }
+           return 0;
+        });
         int subDirTotal = 0;
         for (int i = 0; i < files.length; i++) {
             if (files[i].isDirectory()) {
